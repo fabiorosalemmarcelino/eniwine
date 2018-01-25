6206415a8079937cc8ca9efddda4362d899ad5d0
Fábio Rosalém <fabiorosalem@gmail.com>
2018-01-25 19:42:42 -0200

    first commit

---
+++ EniWine.Investigacao.WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EniWine.Investigacao.WebApplication.MvcApplication" Language="C#" %>
---
+++ EniWine.Investigacao.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EniWine.Investigacao.WebApplication.Startup))]
+namespace EniWine.Investigacao.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
